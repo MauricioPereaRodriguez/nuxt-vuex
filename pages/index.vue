567610a7e04cfeb62411fea55bcd11700e84f463
Mauricio Perea Rodríguez <mperea0@misena.edu.co>
2019-04-18 21:04:33 -0500

    vuex mutations

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,29 @@
     <!--<h1>{{ $store.state.titulo }}</h1>-->
     <h1>{{ titulo }}</h1>
     <h2>{{ subtitulo }}</h2>
+    <button @click="cambioTitulo">Cambiar titulo</button>
+    <br>
+    <input type="text" v-model="sub" placeholder="Cambiar subtitulo">
+    <button @click="cambio">Cambiar subtitulo</button>
   </section>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapMutations } from 'vuex'
 export default {
+  data(){
+    return {
+      sub: ''
+    }
+  },
   computed: {
     ...mapState(['titulo', 'subtitulo'])
+  },
+  methods:{
+    ...mapMutations(['cambioTitulo']),
+    cambio(){
+      this.$store.commit('cambioSubtitulo', this.sub)
+    }
   }
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -2,3 +2,12 @@ export const state = () => ({
   titulo: 'Mi primer nuxt con vuex',
   subtitulo: 'Soy un subtitulo'
 })
+
+export const mutations = {
+  cambioTitulo(state){
+    state.titulo = 'Cambio del titulo'
+  },
+  cambioSubtitulo(state, valor){
+    state.subtitulo = valor
+  }
+}
